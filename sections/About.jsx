7033512882a160da1a7dacff3f89bde35ae345a4
iqbal sahrizal <maniacjoe3@gmail.com>
2022-12-10 03:10:16 +0700

    reset

--- sections/About.jsx
+++ sections/About.jsx
@@ -3,12 +3,7 @@ import { motion } from "framer-motion";
 import { TypingText } from "../components";
 
 import styles from "../styles";
-import {
-  fadeIn,
-  slideIn,
-  staggerContainer,
-  textVariant,
-} from "../utils/motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
